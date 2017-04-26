4bb086fc0c04210e7bad06c4bc2e86abed3b71e2
Aijalon Brito da Silva Junior <juniorsjs6@hotmail.com>
2017-04-26 09:13:51 -0300

    Atualização

--- src/main/java/br/edu/ifbaiano/csi/ngti/cae/controller/AlunosController.java
+++ src/main/java/br/edu/ifbaiano/csi/ngti/cae/controller/AlunosController.java
@@ -1,16 +1,26 @@
 package br.edu.ifbaiano.csi.ngti.cae.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.edu.ifbaiano.csi.ngti.cae.model.Aluno;
+import br.edu.ifbaiano.csi.ngti.cae.service.CadastroAlunoService;
 
 @Controller
 @RequestMapping("/alunos")
 public class AlunosController {
+	
+	@Autowired
+	private CadastroAlunoService cadastroAlunoService;
 
 	@GetMapping
 	public String pesquisar(){
@@ -18,17 +28,35 @@ public class AlunosController {
 	}
 	
 	@GetMapping("/novo")
-	public String novo(){
-		return "aluno/CadastroAluno";
+	public ModelAndView novo(Aluno aluno){
+		ModelAndView mv  = new ModelAndView("aluno/CadastroAluno");
+		mv.addObject("aluno", aluno);
+		
+		return mv;
 	}
 	
 	@PostMapping(value = { "/novo", "{\\d+}" })
-	public String salvar(){
-		return "aluno/CadastroAluno";
+	public ModelAndView salvar(@Valid Aluno aluno, BindingResult result, RedirectAttributes attributs){
+		
+		if(result.hasErrors())
+			return novo(aluno);
+		
+		cadastroAlunoService.salvar(aluno);
+
+		attributs.addFlashAttribute("mensagemSucesso", "Aluno salvo com sucesso!");
+		
+		return new ModelAndView("redirect:/alunos/novo");
 	}
 	
 	@GetMapping("/{codigo}")
-	public String editar(@PathVariable("codigo") Aluno aluno){
-		return "aluno/CadastroAluno";
+	public ModelAndView editar(@PathVariable("codigo") Aluno aluno){
+		/*Aluno aluno = new Aluno();
+		aluno.setCodigo(codigo);
+		aluno.setMatricula("123456");
+		aluno.setNome("nome do aluno");
+		
+		System.out.println("chamou o metodo editar!");
+		System.out.println("codigo do aluno: "+ aluno.getCodigo());*/
+		return novo(aluno);
 	}
 }
--- src/main/java/br/edu/ifbaiano/csi/ngti/cae/model/Aluno.java
+++ src/main/java/br/edu/ifbaiano/csi/ngti/cae/model/Aluno.java
@@ -1,5 +1,46 @@
 package br.edu.ifbaiano.csi.ngti.cae.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+@Entity
+@Table(name="aluno")
 public class Aluno {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+	
+	@NotBlank(message="A matricula é obrigatória")
+	private String matricula;
+	
+	@NotBlank(message="O nome é obrigatório")
+	private String nome;
+	
+	
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+	
+	
 }
