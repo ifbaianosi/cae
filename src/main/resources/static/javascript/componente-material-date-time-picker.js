6794a5a1b622ec33545feb32f367856b2ded4dfe
glaubermatos <glaub.oliveira@hotmail.com>
2017-06-07 22:07:11 -0300

    adicionar mascaras nos inputs e componente de data

--- src/main/resources/static/javascript/componente-material-date-time-picker.js
+++ src/main/resources/static/javascript/componente-material-date-time-picker.js
@@ -7,4 +7,16 @@ $(function(){
         cancelText: 'CANCELAR',
         clearText: 'LIMPAR' 
     });
+	
+	/*$('.datepicker').bootstrapMaterialDatePicker({
+        format: 'DD/MM/YYYY',
+        lang: 'pt-BR',
+        clearButton: true,
+        weekStart: 0,
+        cancelText: 'CANCELAR',
+        clearText: 'LIMPAR' 
+    });*/
+	
+	$('.datepicker').bootstrapMaterialDatePicker({ weekStart : 0, time: false });
 });
+
